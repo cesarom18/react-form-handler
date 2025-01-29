2536de6ebe7e13b4cffcd78bdd341bcfadf8a226
cesarom18 <cesarosses885@gmail.com>
2025-01-29 18:05:27 -0300

    Implementar state de formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import { ChangeEventHandler, MouseEventHandler } from "react";
+import {
+    ChangeEventHandler,
+    MouseEventHandler,
+    useState
+} from "react";
 
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 
+type FormState = {
+    name: string,
+    lastName: string
+}
+
+const initialValue: FormState = {
+    name: "",
+    lastName: ""
+}
+
 export const App = () => {
+    const [form, setForm] = useState(initialValue);
+
     const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        console.log(e.target.value);
+        const name = e.target.name as keyof FormState;
+
+        setForm({ ...form, [name]: e.target.value });
     }
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
         e.preventDefault();
-        console.log("Example");
+
+        setForm(initialValue);
     }
 
     return (
         <div className="hero is-fullheight">
             <div className="hero-body is-flex is-justify-content-center is-align-items-center is-flex-direction-column">
                 <Input
+                    label="Name"
+                    name="name"
                     placeholder="Name"
+                    value={form.name}
+                    handleChange={handleChange} />
+                <Input
+                    label="LastName"
+                    name="lastName"
+                    placeholder="LastName"
+                    value={form.lastName}
                     handleChange={handleChange} />
                 <Button handleClick={handleClick}>Example</Button>
             </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,23 @@
 import { ChangeEventHandler } from "react";
 
 interface InputProps {
+    label: string,
+    name: string,
     placeholder: string,
+    value: string,
     handleChange: ChangeEventHandler<HTMLInputElement>
 }
 
-export const Input = ({ placeholder, handleChange }: InputProps) => {
+export const Input = ({ label, name, placeholder, value, handleChange }: InputProps) => {
     return (
         <div className="field">
-            <label className="label">Name</label>
+            <label className="label">{label}</label>
             <div className="control">
                 <input
                     className="input is-primary"
+                    name={name}
                     placeholder={placeholder}
+                    value={value}
                     onChange={handleChange} />
             </div>
         </div>
