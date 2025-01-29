895340923d8829eafca8cca2fa6da2e4b2b05560
cesarom18 <cesarosses885@gmail.com>
2025-01-29 18:15:40 -0300

    Refactorizacion formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,10 @@
-import {
-    ChangeEventHandler,
-    MouseEventHandler,
-    useState
-} from "react";
-
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
-
-type FormState = {
-    name: string,
-    lastName: string
-}
-
-const initialValue: FormState = {
-    name: "",
-    lastName: ""
-}
+import { UserForm } from "./components/UserForm";
 
 export const App = () => {
-    const [form, setForm] = useState(initialValue);
-
-    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        const name = e.target.name as keyof FormState;
-
-        setForm({ ...form, [name]: e.target.value });
-    }
-
-    const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
-        e.preventDefault();
-
-        setForm(initialValue);
-    }
-
     return (
         <div className="hero is-fullheight">
             <div className="hero-body is-flex is-justify-content-center is-align-items-center is-flex-direction-column">
-                <Input
-                    label="Name"
-                    name="name"
-                    placeholder="Name"
-                    value={form.name}
-                    handleChange={handleChange} />
-                <Input
-                    label="LastName"
-                    name="lastName"
-                    placeholder="LastName"
-                    value={form.lastName}
-                    handleChange={handleChange} />
-                <Button handleClick={handleClick}>Example</Button>
+                <UserForm handleSubmit={user => console.log("Example")} />
             </div>
         </div>
     )
---
+++ src/components/UserForm.tsx
@@ -0,0 +1,58 @@
+import {
+    ChangeEventHandler,
+    MouseEventHandler,
+    useState
+} from "react";
+
+import { Button } from "./Button";
+import { Input } from "./Input";
+
+type UserFormState = {
+    name: string,
+    username: string
+}
+
+const initialValue: UserFormState = {
+    name: "",
+    username: ""
+}
+
+interface UserFormProps {
+    handleSubmit: (user: UserFormState) => void
+}
+
+export const UserForm = ({ handleSubmit }: UserFormProps) => {
+    const [form, setForm] = useState(initialValue);
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        const name = e.target.name as keyof UserFormState;
+
+        setForm({ ...form, [name]: e.target.value });
+    }
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+
+        handleSubmit(form);
+        setForm(initialValue);
+    }
+
+    return (
+        <>
+
+            <Input
+                label="Name"
+                name="name"
+                placeholder="Name"
+                value={form.name}
+                handleChange={handleChange} />
+            <Input
+                label="Username"
+                name="username"
+                placeholder="Username"
+                value={form.username}
+                handleChange={handleChange} />
+            <Button handleClick={handleClick}>Send</Button>
+        </>
+    )
+}
